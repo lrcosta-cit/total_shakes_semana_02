c66dd52afeb734cbb3183c9049bbbae75b998fd5
Lukaz Rocha <lrcosta@ciandt.com>
2022-08-18 14:42:05 -0300

    feat:adicionando pacote armazem e classes/teste

---
+++ src/main/java/br/com/TDD/armazem/Armazem.java
@@ -0,0 +1,11 @@
+package br.com.TDD.armazem;
+
+import br.com.TDD.ingredientes.Ingrediente;
+
+import java.util.TreeMap;
+
+public class Armazem {
+
+    private TreeMap<Ingrediente, Integer> estoque;
+
+}
---
+++ src/test/java/armazem/ArmazemTest.java
@@ -0,0 +1,21 @@
+package armazem;
+
+import br.com.TDD.armazem.Armazem;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ArmazemTest {
+
+    Armazem armazem;
+
+    @BeforeAll
+    public void setup(){
+        armazem = new Armazem();
+    }
+
+    @Test
+    public void adicionarIngredienteTest(){
+
+    }
+
+}

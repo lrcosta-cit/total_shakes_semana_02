904e3172cdb789d22a6e3de9170fdfcf440182c8
Lukaz Rocha <lrcosta@ciandt.com>
2022-08-31 14:58:43 -0300

    fix:ajuste na exceção

--- src/main/java/br/com/TDD/exceptions/DuplicatedIngredienteException.java
+++ src/main/java/br/com/TDD/exceptions/DuplicatedIngredienteException.java
@@ -2,6 +2,10 @@ package br.com.TDD.exceptions;
 
 public class DuplicatedIngredienteException extends RuntimeException{
 
+    public DuplicatedIngredienteException(String message) {
+        super(message);
+    }
+
     public DuplicatedIngredienteException(String message, Throwable cause) {
         super(message, cause);
     }
